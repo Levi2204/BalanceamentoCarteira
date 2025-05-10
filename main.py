c0bf1646fcce401ad1a88188abd654f87a2c058b
Levi2204 <leviteixeira225@gmail.com>
2025-05-09 21:40:23 -0300

    commit correções

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ def percentual(x):
     valor = x *100 / total
     return valor
 
-
+def ideal(ideais, atuais):
+    if atuais == ideais:
+        return "Está balaneceado"
+    elif atuais > ideais:
+        return f"O valor está {atuais - ideais:.2f}% acima do ideal"
+    else:
+        return f"O valor está {ideais - atuais:.2f}% abaixo do ideal "
+def balancear(total, y):
+    porcento = y/100
+    return total * y
 
 total = float(input("Digite quanto você tem na carteira hoje: "))
 
@@ -10,8 +19,9 @@ while True:
     rendaFixa = float(input("Quanto desse valor está na renda fixa: "))
     acoes = float(input("Quanto desse valor está em ações: "))
     fii = float(input("Quanto desse valor está em FIIs: "))
-    if acoes + rendaFixa + fii != total:
-        print("Valores excedem o valor total. Digite novamente")
+    total = rendaFixa + acoes + fii
+    if total != total:
+        print("A soma dos valores não dá o valor total. Digite novamente")
     else:
         break
 
@@ -29,17 +39,11 @@ while True:
     else:
         break
 
-def ideal(ideais, atuais):
-    if atuais == ideais:
-        return "Está balaneceado"
-    elif atuais > ideais:
-        return f"O valor está {atuais - ideais}% acima do ideal"
-    else:
-        return f"O valor está {ideais - atuais}% abaixo do ideal "
 
-print(f"""Ideal renda fixa = {fixaIdeal} | Atual: {divisao["Renda fixa"]} -> {ideal(fixaIdeal, divisao["Renda fixa"])}
-Ideal ações = {acoesIdeal} | Atual: {divisao["Ações"]} -> {ideal(acoesIdeal, divisao["Ações"])}
-Ideal FIIs = {fiiIdeal} | Atual: {divisao["FIIs"]} -> {ideal(fiiIdeal, divisao["FIIs"])}""")
+
+print(f"""Ideal renda fixa = {fixaIdeal:.2f}% | Atual: {divisao["Renda fixa"]:.2f}% -> {ideal(fixaIdeal, divisao["Renda fixa"])}
+Ideal ações = {acoesIdeal:.2f}% | Atual: {divisao["Ações"]:.2f}% -> {ideal(acoesIdeal, divisao["Ações"])}
+Ideal FIIs = {fiiIdeal:.2f}% | Atual: {divisao["FIIs"]:.2f}% -> {ideal(fiiIdeal, divisao["FIIs"])}""")
 
 aporte = float(input("Digite o valor que você irá aportar: "))
 novoTotal = total + aporte
